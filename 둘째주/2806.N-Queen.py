a35577f8f0b75623384177fe226534045bf98956
leesk3732 <leesk3732@naver.com>
2021-10-11 23:45:24 +0900

    211011

--- 둘째주/2806.N-Queen.py
+++ 둘째주/2806.N-Queen.py
@@ -1,3 +1,34 @@
 import sys
 
 sys.stdin = open('2806.txt', 'r')
+
+
+def p(idx, c):
+    for i in range(idx):
+        if idx - i == abs(c - map_list[i]):
+            return True
+    return False
+
+
+def dfs(idx):
+    if idx == N:
+        global ans
+        ans += 1
+        return
+    for i in range(N):
+        if visit[i]: continue
+        if p(idx, i): continue
+        visit[i] = 1
+        map_list[idx] = i
+        dfs(idx + 1)
+        visit[i] = 0
+    return
+
+
+for tc in range(1, int(input()) + 1):
+    N = int(input())
+    map_list = [0 for _ in range(N)]
+    visit = [0] * N
+    ans = 0
+    dfs(0)
+    print('#{} {}'.format(tc, ans))

356e16842dbe9d2ea646ecabc92fb53a5eb9e5af
leesk3732 <leesk3732@naver.com>
2021-10-13 20:19:46 +0900

    1013

---
+++ 둘째주/1493.수의 새로운 연산.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.stdin = open('1493.txt', 'r')
+
+
+def find(t):
+    x = y = 1
+    cur = 1
+    while cur < t:
+        x += 1
+        cur += x
+    back = cur - t
+    x -= back
+    y += back
+    return x, y
+
+
+for tc in range(1, int(input()) + 1):
+    p, q = map(int, input().split())
+    px, py = find(p)
+    qx, qy = find(q)
+
+    ans = step = 1
+    for _ in range(px + qx - 1):
+        step += 1
+        ans += step
+    for _ in range(py + qy - 1):
+        ans += step
+        step += 1
+    print('#{} {}'.format(tc, ans))
